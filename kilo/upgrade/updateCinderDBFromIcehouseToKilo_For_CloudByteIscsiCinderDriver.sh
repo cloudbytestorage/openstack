76c78b3af1126219fd70ef23f6a311f1bcb2483d
Yogesh Prasad <yogesh.prasad@cloudbyte.com>
2015-12-18 16:43:41 +0530

    hide the password when user input

--- kilo/upgrade/updateCinderDBFromIcehouseToKilo_For_CloudByteIscsiCinderDriver.sh
+++ kilo/upgrade/updateCinderDBFromIcehouseToKilo_For_CloudByteIscsiCinderDriver.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
 set -e
 echo -n "Enter Mysql Password > "
+stty -echo
 read pwd
+stty echo
 echo "Started updating cinder database"
 echo "Logging as root"
 mysql -uroot -p$pwd cinder < "updateCinderDBFromIcehouseToKilo_For_CloudByteIscsiCinderDriver.sql"
